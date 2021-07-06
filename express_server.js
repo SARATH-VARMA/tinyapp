04945352a70680422a7f1dc79f64b3b04ea2f3f2
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-07-06 19:04:22 -0400

    Adding post route to handle form submission

--- express_server.js
+++ express_server.js
@@ -3,10 +3,17 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(2,8);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,4 +38,9 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL:  urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  let shortURL = generateRandomString(req.body)
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
